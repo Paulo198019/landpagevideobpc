797de2c4d2be69abe26fd8e44a7dc2d39ae10cab
Paulo198019 <pauloalexandremonteiro89@gmail.com>
2025-12-06 16:25:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const whatsappBtn = document.querySelector('.btn-whatsapp');
+    whatsappBtn.addEventListener('click', () => {
+        alert('Você será redirecionado para o WhatsApp!');
+    });
+});
