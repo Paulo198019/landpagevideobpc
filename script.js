e07e8e90e497ac20abd1b1806825fad2b5a3722d
Paulo198019 <pauloalexandremonteiro89@gmail.com>
2025-12-06 18:45:23 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const whatsappBtn = document.querySelector('.btn-whatsapp');
-    whatsappBtn.addEventListener('click', () => {
-        alert('Você será redirecionado para o WhatsApp!');
-    });
-});
